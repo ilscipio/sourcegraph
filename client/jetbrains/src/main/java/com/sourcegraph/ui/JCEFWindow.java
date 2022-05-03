6cc30766d7f69042939d01cc3fc8affa78a59de8
Paul Piper <pp@ilscipio.com>
2022-05-03 16:00:36 +0200

    Further fixes to browser flickering

--- client/jetbrains/src/main/java/com/sourcegraph/ui/JCEFWindow.java
+++ client/jetbrains/src/main/java/com/sourcegraph/ui/JCEFWindow.java
@@ -9,6 +9,9 @@ import com.intellij.util.ui.UIUtil;
 import com.sourcegraph.scheme.SchemeHandlerFactory;
 import org.cef.CefApp;
 import org.cef.browser.CefBrowser;
+import org.cef.browser.CefFrame;
+import org.cef.handler.CefLoadHandler;
+import org.cef.network.CefRequest;
 
 import javax.swing.*;
 import java.awt.*;
@@ -29,14 +32,13 @@ public class JCEFWindow {
             return;
         }
 
-        this.browser = JBCefBrowser
+        this.browser = new JBCefBrowser("http://sourcegraph/html/index.html")
                 .createBuilder()
-                .setOffScreenRendering(true)
+               .setOffScreenRendering(true)
                 .setUrl("http://sourcegraph/html/index.html")
-                .setCreateImmediately(true)
                 .createBrowser() ;
         this.cefBrowser= browser.getCefBrowser();
-
+        cefBrowser.setWindowVisibility(false);
         CefApp
             .getInstance()
             .registerSchemeHandlerFactory(
@@ -45,10 +47,26 @@ public class JCEFWindow {
                 new SchemeHandlerFactory()
             );
 
+        browser.getJBCefClient().addLoadHandler(new CefLoadHandler() {
+            @Override
+            public void onLoadingStateChange(CefBrowser cefBrowser, boolean isLoading, boolean canGoBack, boolean canGoForward) {
+            }
+            @Override
+            public void onLoadStart(CefBrowser cefBrowser, CefFrame frame, CefRequest.TransitionType transitionType) {
+            }
+            @Override
+            public void onLoadEnd(CefBrowser cefBrowser, CefFrame frame, int httpStatusCode) {
+
+                cefBrowser.setWindowVisibility(true);
+            }
+            @Override
+            public void onLoadError(CefBrowser cefBrowser, CefFrame frame, ErrorCode errorCode, String errorText, String failedUrl) {
+            }
+        }, cefBrowser);
+
         String backgroundColor = "#" + Integer.toHexString(UIUtil.getPanelBackground().getRGB()).substring(2);
         this.browser.setPageBackgroundColor(backgroundColor);
         panel.add(this.browser.getComponent(),BorderLayout.CENTER);
-
         Disposer.register(project, this.browser);
     }
 
